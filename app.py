4573233bc6d44e11b5d4c939a4a7ab16e521d649
nimtas18 <79108051+nimtas18@users.noreply.github.com>
2021-05-11 08:18:34 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST','GET'])
+
+def calculate():
+    bmi=''
+    if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
+        w = float(request.form.get('weight'))
+        h = float(request.form.get('height'))
+        bmi = round(w/((h)**2),2)
+    return render_template('index.html',bmi=bmi)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
